549448c39dc68af48abfae679e34d4603138b6f7
Jakob Coray <jcoray@mit.edu>
2018-10-30 18:32:19 -0400

    made more motor files

--- motorScale.py
+++ motorScale.py
@@ -1,13 +1,18 @@
+# motorScale.py - Scales the impulse of a motor file
+# Created by Jakob Coray (jcoray at mit dot edu) on 10/30/18
+
 import csv
 motorFile = "Rockets_and_motors/CL curve sim.eng"
+initialImpulse = 6.963;
+finalImpulse = 7.6;
 with open(motorFile, 'r') as rf:
-    with open('newmotor.eng', 'w') as wf:
+    with open('Rockets_and_motors/76kns.eng', 'w') as wf:
         reader = csv.reader(rf, delimiter=' ')
         writer = csv.writer(wf, delimiter=' ')
         for row in reader:
             if row[0] != '':
                 writer.writerow(row)
                 continue
-            row[4] = str(float(row[4])*8/6.9)
+            row[4] = str(float(row[4])*finalImpulse/initialImpulse)
             writer.writerow(row)
         
